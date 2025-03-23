619d9c6d9173307f04033302e7ac0ac94ced1fe0
unknown <subhanmaualana14@gmail.com>
2025-03-23 21:06:56 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideoDBC_103022300081 video = new SayaTubeVideoDBC_103022300081("Tutorial Design By Contract - [Subhan Maulana Ahmad]");
+
+        video.PrintVideoDetails();
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+
+        for (int i = 0; i < 10; i++)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+
+        video.PrintVideoDetails();
+
+        Console.ReadLine();
+    }
+}
---
+++ SayaTubeVideoDBC.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+class SayaTubeVideoDBC_103022300081
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideoDBC_103022300081(string title)
+    {
+        Debug.Assert(title != null, "Title tidak bisa kosong/null");
+        Debug.Assert(title.Length <= 100, "Title tidak bisa melebihi 100 kata");
+
+        Random random = new Random();
+        this.id = random.Next(10000, 100000);
+
+        this.title = title;
+
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        Debug.Assert(count <= 10000000, "Count tidak bisa melebihi 10.000.000");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Error: Play count overflow detected");
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
